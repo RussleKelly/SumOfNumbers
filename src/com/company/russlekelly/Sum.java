e4fdceae81b195d09e907dcc3de91ca00dbe89ae
Russle Kelly <Russlegreyson@gmail.com>
2020-04-04 20:57:09 +0300

    Task #1 is done.

---
+++ src/com/company/russlekelly/Sum.java
@@ -0,0 +1,27 @@
+package com.company.russlekelly;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        int total = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number:");
+
+    while(true){
+
+        boolean isAnInt = scanner.hasNextInt();
+
+        if (isAnInt){
+            int number = scanner.nextInt();
+            total+=number;
+        } else {
+            break;
+        }
+
+    }
+        System.out.println("Result is: "+ total);
+
+    }
+}
